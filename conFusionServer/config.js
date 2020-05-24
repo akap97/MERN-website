09f3b489178435cca5e7c1f3d78c971b6b5ff6de
akap97 <rnkap1997@gmail.com>
2020-05-24 14:00:30 +0530

    v1

---
+++ conFusionServer/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb+srv://akap97:1mp00s1b13@cluster0-2zcgi.mongodb.net/test?retryWrites=true&w=majority',
+    //'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '229333041637704',
+        clientSecret: 'de740fcf534a4f98656c5cb66b43303e'
+    }
+}
